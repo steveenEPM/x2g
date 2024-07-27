e2bdfdc78e91a7255c7b173b704201f03051f5c3
Steveen <steveen.pm@gmail.com>
2024-07-27 02:01:32 -0500

    s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable no-unused-vars */
+import { createRoutesFromElements, createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homes from "./pages/index";
+import Actividades from "./pages/actividades";
+import Retos from "./pages/retos";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Homes />,
+    },
+    {
+      path: '/actividades',
+      element: <Actividades />,
+    },
+    {
+      path:'/retos',
+      element:<Retos/>
+    }
+  ])
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
